ef9b2702eadc992d6b2a24125248c558c56e8298
Amit Maity <amit.maity.garia@gmail.com>
2018-05-24 11:58:29 +0530

    Logout logic change

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -64,8 +64,9 @@ class LoginController extends Controller
         if ($user) {
             $user->api_token = null;
             $user->save();
+            return response()->json(['data' => 'User logged out.'], 200);
         }
 
-        return response()->json(['data' => 'User logged out.'], 200);
+        return response()->json(['data' => 'Invalid Token.'], 401);
     }
 }
